81537d4b7133d4e13268cee45bfdfcc203c23c44
Mwilczek00 <mwilczek@gmail.com>
2017-11-13 15:21:48 -0800

    Changed backup and restore scripts
    Broke up the backup and restore
    scripts to beackup the before change
    and after change database.
    Added the same changes to the restore
    scripts.

--- Backup Dev to Before.ps1
+++ Backup Dev to Before.ps1
@@ -2,6 +2,6 @@ Push-Location; Import-Module sqlps -DisableNameChecking; Pop-Location
 
 $serverPath = "SQLSERVER:\SQL\localhost\Default"
 $databaseName = "Payroll"
-$backupTo = join-path (Get-Location) "$databaseName.bak"
+$backupTo = join-path (Get-Location) "$databaseName-Before.bak"
 
 Backup-SqlDatabase -Path $serverPath -Database $databaseName -BackupFile $backupTo
